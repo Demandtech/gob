0433416c56419eea67a31b15c439e88dc3f73003
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-06-11 21:57:12 +0100

    background animation

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,11 @@
-import mp4File from '../assets/fallingstar.mp4'
+
 const Layout = ({ children }) => {
   return (
     <div className='layout md:ml-[100px] min-h-screen relative'>
       <div className='container lg:max-w-5xl lg:mx-auto max-w-7xl  md:px-20 md:15 px-5 pb-20 mt-14 md:mt-0 overflow-y-hidden'>
         {children}
       </div>
-      <div id='video-background'>
-        <video autoPlay loop muted className='h-full object-cover'>
-          <source src={mp4File} type='video/mp4' />
-        </video>
+      <div id='video-background'>    
       </div>
     </div>
   )
